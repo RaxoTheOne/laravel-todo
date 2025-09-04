4c4381fc04075cc46c2d9571eb9eac343568d400
Benjamin Gayda-Knop <traxon.bg@gmail.com>
2025-09-04 21:08:03 +0200

    Aktualisiere Erfolgsmeldungen im Aufgaben-Layout mit Bootstrap-Stilen und füge Schaltfläche zum Schließen hinzu.

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -51,8 +51,9 @@
 
 <!-- Erfolgsmeldung anzeigen -->
 @if (session('success'))
-    <div style="color: green;">
+    <div class="alert alert-success alert-dismissible fade show" role="alert">
         {{ session('success') }}
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
 @endif
 @endsection
