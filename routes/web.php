3e99d409829de607839270ac279c232f6df24398
Benjamin Gayda-Knop <traxon.bg@gmail.com>
2025-09-04 17:54:20 +0200

    Füge eine Route hinzu, um Aufgaben nach Status zu filtern.

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', fn() => redirect()->route('tasks.index'));
 
 Route::resource('tasks', TaskController::class);
+
+// Filter
+Route::get('tasks/filter/{status}', [TaskController::class, 'filter'])->name('tasks.filter');
