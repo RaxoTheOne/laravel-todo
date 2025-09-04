976a76235c6be385dc9f8140252a06fae0bd8adc
Benjamin Gayda-Knop <traxon.bg@gmail.com>
2025-09-04 22:25:24 +0200

    Füge eine zusätzliche Route zum Umschalten des Aufgabenstatus hinzu: Ermögliche die Aktualisierung des Status einer Aufgabe über die neue Route.

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,6 @@ Route::resource('tasks', TaskController::class);
 
 // Filter
 Route::get('tasks/filter/{status}', [TaskController::class, 'filter'])->name('tasks.filter');
+
+// Zusatz-Route zum Status-umschalten
+Route::get('tasks/{task}/status', [TaskController::class, 'updateStatus'])->name('tasks.updateStatus');
